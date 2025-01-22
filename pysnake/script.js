62387fa3d1e65a3387c9f8c3577e85da7ec426d3
Zambesu <114013892+Zambesu@users.noreply.github.com>
2025-01-21 22:33:23 -0300

    Delete pysnake directory

--- pysnake/script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {
-    const nav = document.querySelector('header');
-    if (window.pageYOffset > 0) {
-      nav.classList.add('scrolled');
-    } else {
-      nav.classList.remove('scrolled');
-    }
-  };
-  
